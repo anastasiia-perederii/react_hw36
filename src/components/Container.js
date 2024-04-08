8960d7ffc03c83c8156ac25ea4f69c545c9c5ac3
Anastasiia <anastasiia.perederii@gmail.com>
2024-04-08 14:56:35 +0300

    version1

---
+++ src/components/Container.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Container extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="container">
+                Container
+            </div>
+        );
+    }
+}
+
+export default Container;
---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="navigation">
+                Navigation
+            </div>
+        );
+    }
+}
+
+export default Navigation;
